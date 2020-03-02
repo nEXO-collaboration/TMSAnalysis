20b92e86dda4bb7e32e39dad5d57a16540f3700a
Brian Lenardo <brianlenardo@PHY-C02WH4FHHTD6.local>
2020-03-02 15:58:37 -0800

    Added StruckAnalysisConfiguration class, to read in channel map and such

--- DataReduction/DataReduction.py
+++ DataReduction/DataReduction.py
@@ -1,16 +1,24 @@
 import pandas as pd
 import numpy as np
 import time
+from TMSAnalysis.StruckAnalysisConfiguration import StruckAnalysisConfiguration
 
 from TMSAnalysis.WaveformAnalysis import Waveform
 
-def ReduceH5File( filename, output_dir, num_events=-1, sampling_period=16., input_baseline=-1, input_baseline_rms=-1, polarity=-1., \
-			fixed_trigger=False, trigger_position=0, fit_pulse_flag=False):
+def ReduceH5File( filename, output_dir, run_parameters_file, calibrations_file, channel_map_file, \
+			num_events=-1, input_baseline=-1, input_baseline_rms=-1, \
+			fixed_trigger=False, fit_pulse_flag=False):
 
 	start_time = time.time()
 	filetitle = filename.split('/')[-1]
 	filetitle_noext = filetitle.split('.')[0]
 	outputfile = '{}_reduced.h5'.format(filetitle_noext)
+
+	analysis_config = StruckAnalysisConfiguration()
+	analysis_config.GetRunParametersFromFile( run_parameters_file )
+	analysis_config.GetCalibrationConstantsFromFile( calibrations_file )
+	analysis_config.GetChannelMapFromFile( channel_map_file )
+	sampling_period_ns = 1./(analysis_config.run_parameters['Sampling Rate [MHz]']/1.e3)
 
 	output_series = pd.Series()
 	output_df = pd.DataFrame()
@@ -20,7 +28,9 @@ def ReduceH5File( filename, output_dir, num_events=-1, sampling_period=16., inpu
 	print(input_columns)
 	output_columns = [col for col in input_columns if (col!='Data') and (col!='Channels')]
 
+
 	event_counter = 0
+
 	for index, thisrow in input_df.iterrows():
 		if (event_counter > num_events) and (num_events > 0):
 			break
@@ -40,33 +50,43 @@ def ReduceH5File( filename, output_dir, num_events=-1, sampling_period=16., inpu
 		max_channel_val = 0.
 		# Loop through channels, do the analysis, put this into the output series
 		for ch_num in range(len(thisrow['Channels'])):
+			software_ch_num = thisrow['Channels'][ch_num]
+			#calibration_constant = analysis_config.GetCalibrationConstantForSoftwareChannel( software_ch_num )
+			#decay_constant = analysis_config.GetDecayTimeForSoftwareChannel( software_ch_num )
+			polarity = -1.
+			if analysis_config.run_parameters['Sampling Rate [MHz]'] == 62.5:
+				polarity = 1.
 			w = Waveform.Waveform(input_data=thisrow['Data'][ch_num],\
 						detector_type=thisrow['ChannelTypes'][ch_num],\
-						sampling_period=sampling_period,\
+						sampling_period=sampling_period_ns,\
 						input_baseline=input_baseline,\
 						input_baseline_rms=input_baseline_rms,\
 						polarity=polarity,\
 						fixed_trigger=fixed_trigger,\
-						trigger_position=trigger_position)
+						trigger_position=analysis_config.run_parameters['Pretrigger Length [samples]'],\
+						decay_constant=analysis_config.GetDecayTimeForSoftwareChannel( software_ch_num ),\
+						calibration_constant=analysis_config.GetCalibrationConstantForSoftwareChannel( software_ch_num ) )
 			w.FindPulsesAndComputeArea(fit_pulse_flag=fit_pulse_flag)
 			for key in w.analysis_quantities.keys():
 				output_series['{} {} {}'.format(\
-								thisrow['ChannelTypes'][ch_num],\
-								thisrow['ChannelPositions'][ch_num],\
+								analysis_config.GetChannelTypeForSoftwareChannel( software_ch_num ),\
+								analysis_config.GetChannelNameForSoftwareChannel( software_ch_num ),\
 								key)] = w.analysis_quantities[key]
-			if w.analysis_quantities['Pulse Areas']**2 > 0. and ('TileStrip' in thisrow['ChannelTypes'][ch_num]):
-				output_series['NumTileChannelsHit'] += 1
-				if 'X' in thisrow['ChannelTypes'][ch_num]:
-					output_series['NumXTileChannelsHit'] += 1
-				if 'Y' in thisrow['ChannelTypes'][ch_num]:
-					output_series['NumYTileChannelsHit'] += 1
-				output_series['TotalTileEnergy'] += w.analysis_quantities['Pulse Areas']
-				if w.analysis_quantities['Pulse Areas']**2 > max_channel_val**2:
-					max_channel_val = w.analysis_quantities['Pulse Areas']
-					output_series['TimeOfMaxChannel'] = w.analysis_quantities['Pulse Times']
-			if w.analysis_quantities['Pulse Areas']**2 > 0. and ('SiPM' in thisrow['ChannelTypes']):
-				output_series['NumSiPMChannelsHit'] += 1
-				output_series['TotalSiPMEnergy'] += w.analysis_quantities['Pulse Areas']
+			if 'TileStrip' in analysis_config.GetChannelTypeForSoftwareChannel( software_ch_num ):
+				if w.analysis_quantities['Charge Energy'] > 0.:
+					output_series['NumTileChannelsHit'] += 1
+					if 'X' in analysis_config.GetChannelNameForSoftwareChannel( software_ch_num ):
+						output_series['NumXTileChannelsHit'] += 1
+					if 'Y' in analysis_config.GetChannelNameForSoftwareChannel( software_ch_num ):
+						output_series['NumYTileChannelsHit'] += 1
+					output_series['TotalTileEnergy'] += w.analysis_quantities['Charge Energy']
+					if w.analysis_quantities['Pulse Areas']**2 > max_channel_val**2:
+						max_channel_val = w.analysis_quantities['Charge Energy']
+						output_series['TimeOfMaxChannel'] = w.analysis_quantities['T90']
+			if 'SiPM' in analysis_config.GetChannelTypeForSoftwareChannel( software_ch_num ):
+				if w.analysis_quantities['Pulse Area'] > 0.:
+					output_series['NumSiPMChannelsHit'] += 1
+					output_series['TotalSiPMEnergy'] += w.analysis_quantities['Pulse Areas']
 		# Append this event to the output dataframe
 		output_series['File'] = filetitle
 		output_series['Event'] = event_counter

6a0dfc9b1d91fed71da3a9e206be68758604022c
Jacopo Dalmasson <jacopod@borax2.llnl.gov>
2020-03-16 14:43:09 -0700

    compress into one dataframe

---
+++ scripts/add_into_one_df.py
@@ -0,0 +1,30 @@
+#########################################################################################
+#											#
+#	This script fetches all the reduced .h5 in the folder, producing		#
+#	one dataframe for the run. I noticed that it's faster than loading		#
+#	dynamically each file (in case of the entire run is needed)			#
+#											#
+#					Jacopo						#
+#											#
+#					Usage:						#
+#			python add_into_one_df.py path_to_reduced			#
+#											#
+#########################################################################################
+
+
+import glob, sys, time
+import pandas as pd
+
+start_time = time.time()
+df_list = []
+path_to_folder = sys.argv[1]
+
+if path_to_folder[-1] != '/':
+	path_to_folder += '/'
+
+for i,fname in enumerate(glob.glob(path_to_folder + '*reduced.h5')):
+	if i%100 == 0:
+		print('{} files appeneded in {:.1f}'.format(i,(time.time() - start_time)))
+	df_list.append(pd.read_hdf(fname))
+df = pd.concat(df_list,ignore_index=True)
+df.to_hdf( path_to_folder + 'reduced_added.h5', key='df' )

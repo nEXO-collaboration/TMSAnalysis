be4d0b6c0d213ab0ba7a27e1cb3d52564eca6a2f
Brian Gregory Lenardo <lenardo1@borax2.llnl.gov>
2020-05-18 08:34:22 -0700

    Replaced DataFrame.append() calls with list.append() followed by pandas.concat. This increases the run speed by ~10%

--- DataReduction/DataReduction.py
+++ DataReduction/DataReduction.py
@@ -18,7 +18,8 @@ def ReduceFile( filename, output_dir, run_parameters_file, calibrations_file, ch
         filetitle = filename.split('/')[-1]
         filetitle_noext = filetitle.split('.')[0]
         outputfile = '{}_reduced.h5'.format(filetitle_noext)
-        output_df = pd.DataFrame()
+        output_df_list = [] # For some reason, it's faster to concat a list of dataframes than
+                            # to append dataframes one-by-one.
 
         analysis_config = StruckAnalysisConfiguration.StruckAnalysisConfiguration()
         analysis_config.GetRunParametersFromFile( run_parameters_file )
@@ -33,7 +34,7 @@ def ReduceFile( filename, output_dir, run_parameters_file, calibrations_file, ch
                 n_ev = 0
                 reduced_df = FillH5Reduced(filetitle, input_df, analysis_config, n_ev,\
                                         input_baseline, input_baseline_rms, fixed_trigger, fit_pulse_flag, num_events=-1)
-                output_df = output_df.append(reduced_df,ignore_index=True)
+                output_df_list.append(reduced_df)
         except OSError:
                 # This block runs if the input file is not an HDF5 file (meaning it is
                 # assumed to be a ROOT file).
@@ -67,18 +68,21 @@ def ReduceFile( filename, output_dir, run_parameters_file, calibrations_file, ch
                         
                         reduced_df = FillH5Reduced(filetitle, input_df, analysis_config, n_ev,\
                                                 input_baseline, input_baseline_rms, fixed_trigger, fit_pulse_flag, num_events=-1)
-                        output_df = output_df.append(reduced_df,ignore_index=True)
+                        output_df_list.append(reduced_df)
                         n_ev += 20
 
+        output_df = pd.concat( output_df_list, axis=0, ignore_index=True, sort=False )
         output_df.to_hdf(output_dir + outputfile, key='df')     
         print('Run time: {:4.4}'.format(time.time()-start_time))
 
 
 ##################################################################################################
 def FillH5Reduced(filetitle, input_df, analysis_config, event_counter,\
-                input_baseline, input_baseline_rms, fixed_trigger, fit_pulse_flag, num_events=-1):
+                input_baseline, input_baseline_rms, fixed_trigger, \
+                fit_pulse_flag, num_events=-1):
+
         output_series = pd.Series()
-        output_df = pd.DataFrame()
+        output_list = []
         input_columns = input_df.columns
         output_columns = [col for col in input_columns if (col!='Data') and (col!='Channels')]
         sampling_period_ns = 1./(analysis_config.run_parameters['Sampling Rate [MHz]']/1.e3)
@@ -125,7 +129,7 @@ def FillH5Reduced(filetitle, input_df, analysis_config, event_counter,\
                                 w.FindPulsesAndComputeAQs(fit_pulse_flag=fit_pulse_flag)
                         except IndexError:
                                 print('Null waveform found in channel {}, event {} skipped'.format(ch_num,event_counter))
-                                key_buffer = output_df.keys()
+                                key_buffer = output_list[index-1].keys() # Grab keys from previous event
                                 for key_buf in key_buffer:
                                         output_series[key_buf] = 0
                                 skip = True
@@ -196,8 +200,9 @@ def FillH5Reduced(filetitle, input_df, analysis_config, event_counter,\
                 # Append this event to the output dataframe
                 output_series['File'] = filetitle
                 output_series['Event'] = event_counter
-                output_df = output_df.append(output_series,ignore_index=True)
+                output_list.append(output_series)
                 event_counter += 1
+        output_df = pd.concat( output_list, axis=0, ignore_index=True, sort=False )
         return output_df
 
 
